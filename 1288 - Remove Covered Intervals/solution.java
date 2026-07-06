c556c1b8e1ee3f13b4bfa02bdab005c7791b274d
SAHIL <sahil.md7895322@gmail.com>
2026-07-06 10:00:24 +0530

    Create solution.java

---
+++ 1288 - Remove Covered Intervals/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+        int remainingCount = 0;
+        int maxEnd = 0;
+        
+        for (int[] curr : intervals) {
+            if (curr[1] > maxEnd) {
+                remainingCount++;
+                maxEnd = curr[1];
+            }
+        }
+        return remainingCount;
+    }
+}
