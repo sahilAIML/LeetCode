2b11dfde5ac35719777e337fa840a1407d246d78
SAHIL <sahil.md7895322@gmail.com>
2026-07-29 19:05:25 +0530

    Delete 0448 - Find All Numbers Disappeared in an Array directory

--- 0448 - Find All Numbers Disappeared in an Array/solution.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def findDisappearedNumbers(self, nums: list[int]) -> list[int]:
-        for num in nums:
-            index = abs(num) - 1
-            if nums[index] > 0:
-                nums[index] = -nums[index]
-        return [i + 1 for i, num in enumerate(nums) if num > 0]
