861ed94959a931e7b498c36d4ce681c21ad02e2c
SAHIL <sahil.md7895322@gmail.com>
2026-07-30 23:33:00 +0530

    Create solution.java

---
+++ 1679 - Max Number of K-Sum Pairs/solution.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int left = 0;
+        int right = nums.length-1;
+        int op = 0;
+
+        while(left < right){
+            int sum = nums[left] + nums[right];
+
+            if(sum == k){
+                op++;
+                left++;
+                right--;
+            }
+
+            else if(sum < k){
+                left++;
+            }
+
+            else{
+                right--;
+            }
+        }
+
+        return op;
+    }
+}
